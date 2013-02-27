9261aed0394ebe54d910d578b67d7cb48a70eba0
Naeem Lakhani <naeem@bnotions.com>
2013-02-27 12:28:11 -0500

    Week 1 Initial Commit
    All automatically generated files as well as source file file for Main
    Activity Class

---
+++ week1/HelloWorld/src/com/thinkrs/helloworld/Person.java
@@ -0,0 +1,20 @@
+package com.thinkrs.helloworld;
+
+//a publicly accessible class called Person is created here
+public class Person {
+
+	private String name; //creates a string with variable name 'name'
+	
+	//this block instantiates the Person class and is often called the constructor
+	public Person() { 
+		
+	}
+	
+	//the below function will return the name
+	public String getName() {
+	
+		return name;
+	
+	}
+	
+}
